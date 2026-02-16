d5ba420e78eeb809622d5cd50c7c15c1a6b57210
MusaSagov <sagov82@mail.ru>
2026-02-16 21:37:38 +0300

    fix-6

--- src/interfaces/ISearchForm.ts
+++
@@ -1,8 +0,0 @@
-interface ISearchForm {
-  locationId: number;
-  date: string;
-  participants: number;
-  locationName?: string;
-  participantsCount?: number | string;
-  participantsName: string;
-}
