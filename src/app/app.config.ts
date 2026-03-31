d536460f58acf13f73c1224a90d2473447448476
MusaSagov <sagov82@mail.ru>
2026-03-31 20:44:11 +0300

    fix

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,11 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import { loaderInterceptor } from '../loader/loader.interceptor';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideZoneChangeDetection(),
-    provideHttpClient(withInterceptors([loaderInterceptor]))
+    provideZoneChangeDetection()
   ]
 };
