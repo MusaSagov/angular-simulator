44694dbffc71db9837ddcface83fdcc1c73626ce
MusaSagov <sagov82@mail.ru>
2026-07-10 19:37:09 +0300

    fix

--- src/features/auth/admin.guard.ts
+++ src/features/auth/admin.guard.ts
@@ -2,13 +2,14 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from './auth.service';
 import { Role } from '../../enums/Role';
+import { IAuthUser } from './interfaces';
 
 export const adminGuard: CanActivateFn = () => {
 
-  const authService = inject(AuthService);
-  const router = inject(Router);
+  const authService: AuthService = inject(AuthService);
+  const router: Router = inject(Router);
 
-  const user = authService.getCurrentUser();
+  const user: IAuthUser | null = authService.getCurrentUser();
 
   if (!user) {
     return router.parseUrl('/login');
