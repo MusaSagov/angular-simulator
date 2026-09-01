cddcc3ad96b9da363e3c1c135bc647878444c06c
MusaSagov <sagov82@mail.ru>
2026-09-01 16:40:43 +0300

    fix6

--- src/service/language.service.ts
+++ src/service/language.service.ts
@@ -2,6 +2,7 @@ import { Injectable, inject } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { LocalStorageService } from './local-storage.service';
 import { Language } from '../enums/Language';
+import { PrimeNG } from 'primeng/config';
 
 @Injectable({
   providedIn: 'root',
@@ -10,7 +11,7 @@ export class LanguageService {
 
   private readonly translateService: TranslateService = inject(TranslateService);
   private readonly localStorageService: LocalStorageService = inject(LocalStorageService);
-  
+  private readonly primeNG: PrimeNG = inject(PrimeNG);
   readonly supportedLanguages: Language[] = Object.values(Language);
   currentLanguage: Language = Language.EN;
 
@@ -26,14 +27,16 @@ export class LanguageService {
       language = browserLanguage as Language;
     }
 
-    this.currentLanguage = language;
-    this.translateService.use(language);
+    this.setLanguage(language);
   }
 
   setLanguage(language: Language): void {
     this.currentLanguage = language;
     this.localStorageService.saveData('selected-language', language);
     this.translateService.use(language);
+    this.translateService.get('primeng').subscribe((res) => {
+      this.primeNG.setTranslation(res);
+    });
   }
 
 }
